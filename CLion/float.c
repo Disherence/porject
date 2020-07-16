3bc5c148e593e7c96d24d6bd3d5b493a491f6b7d
于祥麟 <yuxianglin@macbook-pro.lan>
2020-07-17 01:10:41 +0800

    CLion和一些练习

---
+++ CLion/float.c
@@ -0,0 +1,9 @@
+//
+// Created by 于祥麟 on 2020/7/17.
+//
+#include "stdio.h"
+int main()
+{
+    const double RENT = 3852.99;
+    printf("")
+}
---
+++ C_Study/float/float.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+    const double RENT = 3852.99;
+
+    printf
+}
